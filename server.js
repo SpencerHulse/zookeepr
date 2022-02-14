48b7f6d4ba11a7e9b6247ff471827e59004572dd
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-13 22:13:25 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const { animals } = require("./data/animals.json");
@@ -65,6 +67,6 @@ app.get("/api/animals", (req, res) => {
   res.status(200).json(results);
 });
 
-app.listen(3001, (req, res) => {
+app.listen(PORT, (req, res) => {
   console.log("API server now on port 3001!");
 });
