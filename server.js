f4b7ac491e5a99f02df26ee28786ec7da4433686
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-14 02:58:56 -0500

    Added get route and logic for the animals page

--- server.js
+++ server.js
@@ -133,6 +133,22 @@ app.post("/api/animals", (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.listen(PORT, (req, res) => {
   console.log("API server now on port 3001!");
 });
